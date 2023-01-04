58865d33a7538accfd28286ab7155567c7cabaf3
AndrejFellinger <67386846+AndrejFellinger@users.noreply.github.com>
2023-01-04 20:33:14 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let player = null;
-let pc = null;
-let player2 = null;
+let comprandomnumber = null;
+let computer = null;
 let counter1 = 0;
 
 let playername = null;
@@ -29,7 +29,7 @@ function vyhra(){
 }
 
 function prehra(){
-    console.log('player2 WIN');
+    console.log('computer WIN');
     $("#result").text('Prehral si!');
     $("#result").css('color', 'red');
     $("#history").append("<h4>" + counter1 + '. ' + playername + " - Prehra</h4>");
@@ -56,30 +56,33 @@ $(document).ready(function(){
 
     });
 
-    $("#reset").click(function(){
-        location.reload();
-    });
+$("#reset").click(function(){
+    location.reload();
+});
 
-    $("#k").click(function(){
-            player = "K";
-            console.log('hrac:' + player);
-        });
+$("#k").click(function(){
+    player = "K";
+    console.log('hrac:' + player);
+});
 
-        $("#p").click(function(){
-            player = "P";
-            console.log('hrac:' + player);
-        });
+$("#p").click(function(){
+    player = "P";
+    console.log('hrac:' + player);
+});
 
-        $("#n").click(function(){
-            player = "N";
-            console.log('hrac:' + player);
-        });
+$("#n").click(function(){
+    player = "N";
+    console.log('hrac:' + player);
+});
 
-    $(".selectbtn").click(function(){
+$(".selectbtn").click(function(){
 
         playername = $("#player-name").val();
-        if(playername != ""){
-            
+        
+        if(playername == ""){
+            return(alert('nastav si meno'));
+
+        }else{
             console.log("SELECT");
             $('#player-name-sc').text(playername);
             $('#scores').css('display', 'block');
@@ -87,64 +90,64 @@ $(document).ready(function(){
 
             counter1 ++;
 
-            pc = Math.floor(Math.random() * 3);
-            console.log(pc + 1);
+            comprandomnumber = Math.floor(Math.random() * 3);
+            console.log(comprandomnumber);
 
-            if(pc + 1 == 1){
-                player2 = "K";
+            if(comprandomnumber == 0){
+                computer = "K";
             }
-            else if(pc + 1 == 2){
-                player2 = "P";
+            else if(comprandomnumber == 1){
+                computer = "P";
             }
-            else if(pc + 1 == 3){
-                player2 = "N";
+            else if(comprandomnumber == 2){
+                computer = "N";
             }
 
-            console.log('botko:' + player2);
+            console.log('botko:' + computer);
 
-            if(player == "K" && player2 == "K"){
+            if(player == "K" && computer == "K"){
                 remiza();
                 $("#values").text(playername +': Kamen, Bot: Kamen');
             }
 
-            else if(player == "P" && player2 == "P"){
+            else if(player == "P" && computer == "P"){
                 remiza();
                 $("#values").text(playername +': Papier, Bot: Papier');
             }
 
-            else if(player == "N" && player2 == "N"){
+            else if(player == "N" && computer == "N"){
                 remiza();
                 $("#values").text(playername +': Noznice, Bot: Noznice');
             }
 
 
-            if(player == "K" && player2 == "N"){
+            if(player == "K" && computer == "N"){
                 vyhra();
                 $("#values").text(playername +': Kamen, Bot: Noznice');
             }
 
-            else if(player == "P" && player2 == "K"){
+            else if(player == "P" && computer == "K"){
                 vyhra();
                 $("#values").text(playername +': Papier, Bot: Kamen');
             }
 
-            else if(player == "N" && player2 == "P"){
+            else if(player == "N" && computer == "P"){
                 vyhra();
                 $("#values").text(playername +': Noznice, Bot: Papier');
             }
 
 
-            if(player == "P" && player2 == "N"){
+            if(player == "P" && computer == "N"){
                 prehra();
                 $("#values").text(playername +': Papier, Bot: Noznice');
             }
 
-            else if(player == "N" && player2 == "K"){
+            else if(player == "N" && computer == "K"){
                 prehra();
                 $("#values").text(playername +': Noznice, Bot: Kamen');
             }
 
-            else if(player == "K" && player2 == "P"){
+            else if(player == "K" && computer == "P"){
                 prehra();
                 $("#values").text(playername +': Kamen, Bot: Papier');
             }
@@ -154,9 +157,6 @@ $(document).ready(function(){
             $("#player-name").css('display', 'none');
             $("#status").css('display', 'block');
             $("#next").css('display', 'block');
-
-        }else{
-            alert("Nastav si meno");
         }
 
 
